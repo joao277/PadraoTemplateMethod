a3068a1cb85cd2f039cbcf3a0508175bdfe30d56
joão277 <joaobstarocha@gmail.com>
2023-06-13 22:25:04 -0300

    Complete.

---
+++ demo/src/main/java/com/example/PlayerHunter.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class PlayerHunter extends Player{
+    private Integer MAX_DAMAGE_HUNTER = 200;
+
+    @Override
+    public Boolean checkIfHasMaxDamage() {
+        if(this.getFullDamage() >= MAX_DAMAGE_HUNTER){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
